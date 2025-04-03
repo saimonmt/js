9de19e87d4247b36ca15ebbc276eeb36363ae2b2
saimonmt <saimonfer@gmail.com>
2025-04-03 11:32:08 -0400

    Aula 1 JS
    Aula 1 JS 03/04/2025

---
+++ aula1.js
@@ -0,0 +1,29 @@
+var nome = "Joao";
+let idade = 25;
+const PI = 3.14;
+
+let numero = 42;
+let Texto = "Isso e uma string";
+let isVerdadeiro = true;
+let frutas = ["maca","banana","laranja"];
+
+
+let abacaxi = 10 
+let limao = 5
+let resultado
+let sub
+let div
+let mult
+const laranja = "joao"
+console.log(abacaxi)
+
+resultado = abacaxi + limao
+sub = abacaxi - limao
+div = abacaxi / limao
+mult = abacaxi * limao
+
+console.log("Soma = " + resultado)
+console.log("Subtracao = " + sub)
+console.log("Divisao = " + div)
+console.log("Multiplicacao = " + mult)
+
